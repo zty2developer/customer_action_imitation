6c74fe3ead0063cccb075950a250617842ca9eca
Alec Zhou <ztyemail@126.com>
2019-08-27 10:18:06 +0800

    完善了创建用户的接口

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def create
     user_login = params[:login]
     if User.where(login: user_login).exists?
       render json: {status: -1, message: 'User ID Exists.'}
     else
-      render json: {status: 0, messgae: 'Success!'}
+      user = User.new(login: user_login)
+      user.save
+      render json: {status: 0, user_id: user.id}
     end
   end
 end
