6ec52d7cf2270af09faf71cc8721dd97cc71056b
Alec Zhou <ztyemail@126.com>
2019-08-31 18:06:01 +0800

    add some spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   after_action :set_access_control_headers
 
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin']='*'
-    headers['Access-Control-Allow-Methods']='POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method']='*'
-    headers['Access-Control-Allow-Headers']='Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
