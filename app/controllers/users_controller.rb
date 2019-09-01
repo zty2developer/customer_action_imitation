a80c24278f650be7de0f89a44bc71de5c4a1e9df
Alec Zhou <ztyemail@126.com>
2019-09-01 11:16:00 +0800

    same user id, different page. is allowed now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,14 @@ class UsersController < ApplicationController
       render json: { status: -1, message: 'login and with_prediction parameter required.' }
       return
     end
+    with_prediction = with_prediction.to_i
 
-    if User.where(login: user_login).exists?
-      render json: {status: -1, message: 'User ID Exists.'}
+    if User.where(login: user_login, with_prediction: with_prediction).exists?
+      render json: {status: -1, message: "User ID Exists. (with_prediction: #{with_prediction})"}
     else
       user = User.new(login: user_login)
-      if [0, 1, 2].include?(with_prediction.to_i)
-        user.with_prediction = with_prediction.to_i
+      if [0, 1, 2].include?(with_prediction)
+        user.with_prediction = with_prediction
       else
         render json: { status: -1, message: 'parameter with_prediction is incorrect.' }
         return
